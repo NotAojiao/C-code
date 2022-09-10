6bc0cbb43c553cf14ebd84072ce0807719e10fe1
NotAojiao <113228429+NotAojiao@users.noreply.github.com>
2022-09-10 21:10:25 +0800

    Add files via upload

---
+++ test0101.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a = 'a';
+	cout << "ÇëÊäÈë" << endl;
+
+	cin >> a;
+	switch (a) 
+	{
+	case 'b':
+		cout << "ÎÒ²âÄãÃÇÂë" << endl;
+		break;
+
+
+
+
+	}
+	
+
+	return 0;
+}
